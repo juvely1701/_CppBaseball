5efaee90263ce20a2fa8a4ff4e2cb1f53ce92d58
cplkimth <cplkimth@naver.com>
2019-10-08 11:54:26 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,75 +1,46 @@
 #include <iostream>
 #include <cstdlib>
-#include "Result.h"
-
 using namespace std;
 
-static const int DIGIT = 3;
-static const int MAX_NUMBER = 10;
+int* take1()
+{
+    int ar[2];
+    ar[0] = 1;
+    ar[1] = 2;
 
-void printNumbers(const char* prefix, int* numbers) {
-    cout << prefix << endl;
-    for (int i = 0; i < DIGIT; ++i)
-        cout << numbers[i] << ' ';
-    cout << endl;
+    return ar;
 }
 
-void createAnswers(int* answers){
-    while (true) {
-        for (int i = 0; i < DIGIT; ++i)
-            answers[i] = rand() % MAX_NUMBER;
+int* take2()
+{
+    int size = rand() % 10 + 1;
 
-        //TODO : 까다로우니까 나중에 검토
-        if (answers[0] != answers[1] && answers[1] != answers[2] && answers[2] != answers[0])
-            break;
-    }
+    int* ar = (int*)malloc(sizeof(int) * size);
+    for (int i = 0; i < size; ++i)
+        ar[i] = i + 1;
 
-    printNumbers("[ANSWER]", answers);
+    return ar;
+}
+
+void take4(int* ar){
+    int size = rand() % 10 + 1;
+
+    for (int i = 0; i < size; ++i)
+        ar[i] = i + 1;
 }
 
 int main() {
-    // 1. 0~9 사이의 중복되지 않는 난수 3가지를 골라서 정답을 생성
-    int answers[DIGIT];
-    createAnswers(answers);
+    int* t1 = take1();
+    cout << *(t1 + 0) << endl;
 
-    int tryCount = 0;
+    int* t2 = take2();
+    cout << *(t2 + 0) << endl;
+    free(t2);
 
-    while (true) {
-        tryCount++;
+    int t4[10];
+    take4(t4);
+    cout << *(t4 + 0) << endl;
 
-        // 2. 사용자로부터 3개의 숫자(추측)를 입력 받음
-        int guesses[DIGIT];
-        inputGuesses(guesses);
-
-        // 3. 정답과 추측을 비교하여 결과 판정
-        Result result;
-        result.strike = 0;
-        result.ball = 0;
-        result.out = 0;
-
-        for (int i = 0; i < DIGIT; ++i){
-            int j = (i + 1) % DIGIT;
-            int k = (i + 2) % DIGIT;
-
-            if (guesses[i] == answers[i])
-                result.strike++;
-            else if (guesses[i] == answers[j] || guesses[i] == answers[k])
-                result.ball++;
-            else
-                result.out++;
-        }
-
-        // 4. 결과를 화면에 출력
-        // [RESULT] S:1 B:2 O:0
-        cout << "[RESULT] S:" << result.strike << " B:" << result.ball << " O:" << result.out << endl;
-
-        // 5. 추측이 결과와 다르면 2번 단계로 돌아가서 반복
-        if (result.strike == DIGIT)// magic number, hard-coded number, 0/1 예외
-            break;
-    }
-
-    // 6. 정답을 맞추는데 소요된 횟수를 출력하고 종료
-    cout << "[COUNT] " << tryCount << endl;
 
     return 0;
 }
